80642d070efdcc85db46d500c150ca155795bcc7
Zakrzewiaczek <93476294+Zakrzewiaczek@users.noreply.github.com>
2024-01-22 08:23:47 +0100

    Update code.ps1

--- code.ps1
+++ code.ps1
@@ -2,7 +2,7 @@ function map($value, $fromLow, $fromHigh, $toLow, $toHigh) {
     return ($value - $fromLow) * ($toHigh - $toLow) / ($fromHigh - $fromLow) + $toLow
 }
 
-function tworzenie_skoroszytu {
+function make_worksheet {
     param (
         [string]$path,
         [string]$worksheet_name
@@ -36,23 +36,23 @@ $range.RowHeight = 30
 $worksheet.Cells.Item(1, 1).Orientation = 90
 $worksheet.Cells.Item(1, 1).Value2 = Get-Date -Format "dd MMMM yyyy  r."
 
-$worksheet.Cells.Item(1, 2).Value2 = "Godzina"
-$worksheet.Cells.Item(1, 3).Value2 = "Pogoda"
-$worksheet.Cells.Item(1, 4).Value2 = "Temperatura"
-$worksheet.Cells.Item(1, 5).Value2 = "Temp. odczuwalna"
-$worksheet.Cells.Item(1, 6).Value2 = "Wilgotność"
-$worksheet.Cells.Item(1, 7).Value2 = "Ciśnienie"
-$worksheet.Cells.Item(1, 8).Value2 = "Prędkość wiatru"
-$worksheet.Cells.Item(1, 9).Value2 = "Kierunek wiatru"
-$worksheet.Cells.Item(1, 10).Value2 = "Rodzaj chmur"
-$worksheet.Cells.Item(1, 11).Value2 = "Stopień zachmurzenia"
-$worksheet.Cells.Item(1, 12).Value2 = "Poziom UV"
-$worksheet.Cells.Item(1, 13).Value2 = "Gazy: CO2"
-$worksheet.Cells.Item(1, 14).Value2 = "Gazy: NO2"
-$worksheet.Cells.Item(1, 15).Value2 = "Gazy: O3"
-$worksheet.Cells.Item(1, 16).Value2 = "Gazy: SO2"
-$worksheet.Cells.Item(1, 17).Value2 = "Gazy: PM2.5"
-$worksheet.Cells.Item(1, 18).Value2 = "Gazy: PM10"
+$worksheet.Cells.Item(1, 2).Value2 = "Time"
+$worksheet.Cells.Item(1, 3).Value2 = "Weather"
+$worksheet.Cells.Item(1, 4).Value2 = "Temperature"
+$worksheet.Cells.Item(1, 5).Value2 = "Feels like "
+$worksheet.Cells.Item(1, 6).Value2 = "Humidity"
+$worksheet.Cells.Item(1, 7).Value2 = "Pressure"
+$worksheet.Cells.Item(1, 8).Value2 = "Wind speed"
+$worksheet.Cells.Item(1, 9).Value2 = "Wind direction"
+$worksheet.Cells.Item(1, 10).Value2 = "Clouds type"
+$worksheet.Cells.Item(1, 11).Value2 = "Cloud cover"
+$worksheet.Cells.Item(1, 12).Value2 = "UV Level"
+$worksheet.Cells.Item(1, 13).Value2 = "Gases: CO2"
+$worksheet.Cells.Item(1, 14).Value2 = "Gases: NO2"
+$worksheet.Cells.Item(1, 15).Value2 = "Gases: O3"
+$worksheet.Cells.Item(1, 16).Value2 = "Gases: SO2"
+$worksheet.Cells.Item(1, 17).Value2 = "Gases: PM2.5"
+$worksheet.Cells.Item(1, 18).Value2 = "Gases: PM10"
 
 $border = $worksheet.Range("A1:R6").Borders
 $border.LineStyle = 1
@@ -73,19 +73,19 @@ $excel.Quit()
 Remove-Variable excel
 }
 
-function otwieranie_excel_wpisywanie {
+function open_and_write {
     param (
         [string]$path,
-        [int]$rzad,
-        [string]$worksheet_name
+        [int]$row,
+        [string]$worksheet_name,
+        [string]$APIKey,
+        [string]$City
     )
 
 $excel = New-Object -ComObject Excel.Application
 $workbook = $excel.Workbooks.Open($path)
 $worksheet = $workbook.Worksheets.Item($worksheet_name)
 
-$APIKey = "7aeb5673292d463786164127241401"
-$City = "Nasielsk"
 $URL = "https://api.weatherapi.com/v1/current.json?key=$APIKey&q=$City&aqi=yes&lang=pl"
 
 $WeatherData = Invoke-RestMethod -Uri $URL -Method Get
@@ -98,48 +98,48 @@ $web = "https:" + ($WeatherData.current.condition.icon)
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($web, "$home\icon.jpg")
 
-$picture = $worksheet.Shapes.AddPicture(("$home\icon.jpg"), $false, $true, $range.Left + 164, $range.Top - 3 + ($rzad * 30), 30, 30)
+$picture = $worksheet.Shapes.AddPicture(("$home\icon.jpg"), $false, $true, $range.Left + 164, $range.Top - 3 + ($row * 30), 30, 30)
 
-$worksheet.Cells.Item($rzad + 1, 4).Value2 = $WeatherData.current.temp_c.ToString() + "°C"
-$worksheet.Cells.Item($rzad + 1, 5).Value2 = $WeatherData.current.feelslike_c.ToString() + "°C"
+$worksheet.Cells.Item($row + 1, 4).Value2 = $WeatherData.current.temp_c.ToString() + "°C"
+$worksheet.Cells.Item($row + 1, 5).Value2 = $WeatherData.current.feelslike_c.ToString() + "°C"
 
 #kolory temp.
 
-if ($WeatherData.current.temp_c -lt 0) { $worksheet.Cells.Item($rzad + 1, 4).Interior.Color = 15917529 } #D9E1F2
-if ($WeatherData.current.temp_c -gt 0) { $worksheet.Cells.Item($rzad + 1, 4).Interior.Color = 14277119 } #FFD9D9
+if ($WeatherData.current.temp_c -lt 0) { $worksheet.Cells.Item($row + 1, 4).Interior.Color = 15917529 } #D9E1F2
+if ($WeatherData.current.temp_c -gt 0) { $worksheet.Cells.Item($row + 1, 4).Interior.Color = 14277119 } #FFD9D9
 
-if ($WeatherData.current.feelslike_c -lt 0) { $worksheet.Cells.Item($rzad + 1, 5).Interior.Color = 15917529 } #D9E1F2
-if ($WeatherData.current.feelslike_c -gt 0) { $worksheet.Cells.Item($rzad + 1, 5).Interior.Color = 14277119 } #FFD9D9
+if ($WeatherData.current.feelslike_c -lt 0) { $worksheet.Cells.Item($row + 1, 5).Interior.Color = 15917529 } #D9E1F2
+if ($WeatherData.current.feelslike_c -gt 0) { $worksheet.Cells.Item($row + 1, 5).Interior.Color = 14277119 } #FFD9D9
 
 ############
 
-$worksheet.Cells.Item($rzad + 1, 6).Value2 = $WeatherData.current.humidity.ToString() + "%"
+$worksheet.Cells.Item($row + 1, 6).Value2 = $WeatherData.current.humidity.ToString() + "%"
 
 #kolor hum.
 
 $r = [int](map $WeatherData.current.humidity 0 100 255 65)
 
-$worksheet.Cells.Item($rzad + 1, 6).Interior.Color = $r + ($r * 256) + 16711680
+$worksheet.Cells.Item($row + 1, 6).Interior.Color = $r + ($r * 256) + 16711680
 
 if ($WeatherData.current.humidity -gt 50) { 
-   $worksheet.Cells.Item($rzad + 1, 6).font.ColorIndex = 2; 
-   $worksheet.Cells.Item($rzad + 1, 6).font.bold = $True 
+   $worksheet.Cells.Item($row + 1, 6).font.ColorIndex = 2; 
+   $worksheet.Cells.Item($row + 1, 6).font.bold = $True 
 }
 
 ############
 
-$worksheet.Cells.Item($rzad + 1, 7).Value2 = $WeatherData.current.pressure_mb.ToString() + " hPa"
-$worksheet.Cells.Item($rzad + 1, 8).Value2 = $WeatherData.current.wind_kph.ToString() + " km/h"
-$worksheet.Cells.Item($rzad + 1, 9).Value2 = $WeatherData.current.wind_degree.ToString() + "° (" + $WeatherData.current.wind_dir.ToString() + ")"
-$worksheet.Cells.Item($rzad + 1, 10).Value2 = "Rodzaj chmur"
-$worksheet.Cells.Item($rzad + 1, 11).Value2 = $WeatherData.current.cloud.ToString() + "%"
-$worksheet.Cells.Item($rzad + 1, 12).Value = $WeatherData.current.uv
-$worksheet.Cells.Item($rzad + 1, 13).Value = $WeatherData.current.air_quality.co
-$worksheet.Cells.Item($rzad + 1, 14).Value = $WeatherData.current.air_quality.no2
-$worksheet.Cells.Item($rzad + 1, 15).Value = $WeatherData.current.air_quality.o3
-$worksheet.Cells.Item($rzad + 1, 16).Value = $WeatherData.current.air_quality.so2
-$worksheet.Cells.Item($rzad + 1, 17).Value = $WeatherData.current.air_quality.pm2_5
-$worksheet.Cells.Item($rzad + 1, 18).Value = $WeatherData.current.air_quality.pm10
+$worksheet.Cells.Item($row + 1, 7).Value2 = $WeatherData.current.pressure_mb.ToString() + " hPa"
+$worksheet.Cells.Item($row + 1, 8).Value2 = $WeatherData.current.wind_kph.ToString() + " km/h"
+$worksheet.Cells.Item($row + 1, 9).Value2 = $WeatherData.current.wind_degree.ToString() + "° (" + $WeatherData.current.wind_dir.ToString() + ")"
+$worksheet.Cells.Item($row + 1, 10).Value2 = "Rodzaj chmur"
+$worksheet.Cells.Item($row + 1, 11).Value2 = $WeatherData.current.cloud.ToString() + "%"
+$worksheet.Cells.Item($row + 1, 12).Value = $WeatherData.current.uv
+$worksheet.Cells.Item($row + 1, 13).Value = $WeatherData.current.air_quality.co
+$worksheet.Cells.Item($row + 1, 14).Value = $WeatherData.current.air_quality.no2
+$worksheet.Cells.Item($row + 1, 15).Value = $WeatherData.current.air_quality.o3
+$worksheet.Cells.Item($row + 1, 16).Value = $WeatherData.current.air_quality.so2
+$worksheet.Cells.Item($row + 1, 17).Value = $WeatherData.current.air_quality.pm2_5
+$worksheet.Cells.Item($row + 1, 18).Value = $WeatherData.current.air_quality.pm10
 
 del "$home\icon.jpg"
 
@@ -150,7 +150,7 @@ $excel.Quit()
 Remove-Variable excel
 }
 
-function nowy_worksheet {
+function new_page {
 param (
         [string]$path,
         [string]$worksheet_name
@@ -173,10 +173,10 @@ function wait_until_hour {
 
 param (
         [int]$wait_until_hour,
-        [bool]$czekanie_do
+        [bool]$waiting_until
     )
 
-if($czekanie_do) {
+if($waiting_until) {
     while ((Get-Date).Hour -ne $wait_until_hour) {
         Start-Sleep -Seconds 10
     }
@@ -201,30 +201,34 @@ if (-not (Test-Path -Path $folder_name -PathType Container)) {
 Set-Location -Path $folder_name
 }
 
-function wpisz_dane {
+function enter_data {
 
 param (
-        [int]$rzad
+        [int]$row,
+        [string]$worksheet_name
     )
 
 if (-not (Test-Path (Join-Path (Get-Location) ((Get-Date).Day.ToString() + ".xlsx")))) {
-    tworzenie_skoroszytu -path (Join-Path (Get-Location) ((Get-Date).Day.ToString() + ".xlsx")) -worksheet_name "Dane"
+    make_worksheet -path (Join-Path (Get-Location) ((Get-Date).Day.ToString() + ".xlsx")) -worksheet_name $worksheet_name
 }
 
-otwieranie_excel_wpisywanie -path (Join-Path (Get-Location) ((Get-Date).Day.ToString() + ".xlsx")) -worksheet_name "Dane" -rzad $rzad
+open_and_write -path (Join-Path (Get-Location) ((Get-Date).Day.ToString() + ".xlsx")) -worksheet_name $worksheet_name -rzad $row -APIKey 7aeb5673292d463786164127241401 -City Nasielsk
 
 }
 
-##########   Code   ##########
+##########   Master code   ##########
+
+Set-Location -Path "$home\Desktop"
+go_to_folder -folder_name "Weather"
 
 while($true) {
 
 for ($i = 5; $i -le 21; $i += 4) {
-    wait_until_hour -wait_until_hour $i -czekanie_do $true
-    wpisz_dane -rzad (($i-1)/4)
-    wait_until_hour -wait_until_hour $i -czekanie_do $false
+    wait_until_hour -wait_until_hour $i -waiting_until $true
+    enter_data -rzad (($i-1)/4)
+    wait_until_hour -wait_until_hour $i -waiting_until $false
 
-    Set-Location -Path "$home\Desktop\Pogoda"
+    Set-Location -Path "$home\Desktop\Weather"
 
     go_to_folder -folder_name ((Get-Date).Year)
     go_to_folder -folder_name (Get-Date -Format "MMMM")
